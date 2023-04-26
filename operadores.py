337a1ea08b36ccabe7bbe9f2a62bdffd30b1135d
unknown <ccerdapavez@gmail.com>
2023-04-26 17:08:37 -0400

    first commit

---
+++ operadores.py
@@ -0,0 +1,29 @@
+# operadores matematicos
+# realizar la suma
+# datos de entrada
+variable1= int(input("ingrese el primer número: "))
+variable2= int(input("ingrese el segundo número: "))
+               
+# procesando los datos
+suma = variable1 + variable2
+resta = variable1 - variable2
+multiplicación = variable1 * variable2
+división = variable1 / variable2
+modulo= variable1 % variable2
+potencia= variable1 ** variable2
+
+# salida del proceso
+
+print(suma)
+print(resta)
+print(multiplicación)
+print(división)
+print(modulo)
+print(potencia)
+# salida con interpolación
+print(f"la suma es : {suma}")
+print(f"la resta es : {resta}")
+print(f"la multiplicación es : {multiplicación}")
+print(f"la división es : {división}")
+print(f"el modulo es : {modulo}")
+print(f"la potencia es : {potencia}")
---
+++ variables.py
@@ -0,0 +1,11 @@
+# declarando variables
+nombre = "carla"
+edad = 38
+valor_dolar = 810.20
+estudias = True
+
+# imprimiendo nuestras variables 
+print (nombre)
+print (edad)
+print (valor_dolar)
+print (estudias)
